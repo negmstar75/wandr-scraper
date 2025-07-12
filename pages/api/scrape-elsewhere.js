f6887867d6c9626fa79f60cf6fb4176d85cbf71b
negmstar75 <negmstar@gmail.com>
2025-07-12 19:26:45 +0300

    Delete pages/api/scrape-elsewhere.js

--- pages/api/scrape-elsewhere.js
+++
@@ -1,8 +0,0 @@
-import { runElsewhereScraper } from '../../scrapers/elsewhereItineraries.js';
-
-export default async function handler(req, res) {
-  if (req.method !== 'POST') return res.status(405).json({ error: 'Method not allowed' });
-
-  await runElsewhereScraper();
-  return res.status(200).json({ status: 'ok', source: 'elsewhere' });
-}
